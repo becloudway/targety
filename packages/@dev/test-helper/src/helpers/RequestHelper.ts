70f193340732e8405abc90f096a065cc84c9952b
Mitch Mommers <mitchmommers1@gmail.com>
2021-10-22 08:15:30 +0200

    resolved recursive dependencies

--- packages/@dev/test-helper/src/helpers/RequestHelper.ts
+++ packages/@dev/test-helper/src/helpers/RequestHelper.ts
@@ -1,5 +1,3 @@
-import { LambdaProxyEvent, Request } from "targety";
-
 import set from "lodash.set";
 
 import { ApiGateWayProxyEvent } from "../fixtures";
@@ -7,8 +5,8 @@ import { ApiGateWayProxyEvent } from "../fixtures";
 /**
  * Returns a dummy event from the ApiGateWayProxyEvent classes
  */
-export const getDummyEvent = (input: ApiGateWayProxyEvent.ProxyEventInput): LambdaProxyEvent =>
-    ApiGateWayProxyEvent.get(input) as unknown as LambdaProxyEvent;
+export const getDummyEvent = (input: ApiGateWayProxyEvent.ProxyEventInput): unknown =>
+    ApiGateWayProxyEvent.get(input) as unknown as unknown;
 
 /**
  * Create an event object based on the input values
@@ -20,30 +18,14 @@ export const getDummyEvent = (input: ApiGateWayProxyEvent.ProxyEventInput): Lamb
  * @param input any additional input for the proxy event
  */
 export const createTestEvent = (
-    values: Partial<LambdaProxyEvent>,
+    values: Partial<unknown>,
     input: ApiGateWayProxyEvent.ProxyEventInput = {},
-): LambdaProxyEvent => {
+): unknown => {
     const event = getDummyEvent(input);
 
     Object.keys(values).forEach((v) => {
-        set(event, v, values[v]);
+        set(event as any, v, values[v]);
     });
 
     return event;
-};
-
-/**
- * Create a Request object based on the input values
- * Input values keys are used as paths to set the value of the object
- * lodash set function is used to set these paths and will create them if they don't
- * exist
- *
- * @param values object using lodash (set) paths and values
- * @param input any additional input for the proxy event
- */
-export const createTestRequest = (
-    values: Partial<LambdaProxyEvent>,
-    input: ApiGateWayProxyEvent.ProxyEventInput = {},
-): Request => {
-    return new Request(createTestEvent(values, input));
 };
